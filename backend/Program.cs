22e6eeb6cada8186314b9808c6bee273bb80c7a4
Giovanni <giovannivalieri5@gmail.com>
2025-05-05 18:56:53 -0300

    cleanup: remoção de comentários padrão

--- backend/Program.cs
+++ backend/Program.cs
@@ -7,15 +7,10 @@ namespace backend
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-
             builder.Services.AddControllers();
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
             builder.Services.AddOpenApi();
 
             var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.MapOpenApi();
