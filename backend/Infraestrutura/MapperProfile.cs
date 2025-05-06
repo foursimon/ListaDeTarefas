94aace72f71e63c609080f0f3c905d7680eda9b4
Giovanni <giovannivalieri5@gmail.com>
2025-05-06 13:54:51 -0300

    build: configuração de mapeamento adicionado

---
+++ backend/Infraestrutura/MapperProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using backend.Models.Dtos;
+using backend.Models.Entities;
+
+namespace backend.Infraestrutura
+{
+	public class MapperProfile : Profile
+	{
+		public MapperProfile() 
+		{
+			CreateMap<UsuarioCreate, Usuario>();
+		}
+	}
+}
