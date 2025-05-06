858774aef3cbc9d37229863a42781f7dabacb1c6
Giovanni <giovannivalieri5@gmail.com>
2025-05-06 14:11:03 -0300

    feat: Classe para logar na conta de usuário adicionada

---
+++ backend/Models/Dtos/UsuarioDto/UsuarioLogin.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace backend.Models.Dtos.UsuarioDto
+{
+	public class UsuarioLogin
+	{
+		[Required(ErrorMessage = "insira seu email")]
+		public required string Email { get; set; }
+		[Required(ErrorMessage = "insira sua senha")]
+		public required string Senha { get; set; }
+	}
+}
