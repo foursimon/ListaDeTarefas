90f0a0643a8a8520ff45c325baeedf6901397e2b
Giovanni <giovannivalieri5@gmail.com>
2025-05-06 13:55:33 -0300

    feat: Dto para criar conta adicionado

---
+++ backend/Models/Dtos/UsuarioCreate.cs
@@ -0,0 +1,15 @@
+﻿using backend.Models.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace backend.Models.Dtos
+{
+	public class UsuarioCreate
+	{
+		[Required(ErrorMessage = "Insira seu nome")]
+		public required string Nome { get; set; }
+		[Required(ErrorMessage = "Insira seu e-mail")]
+		public required string Email { get; set; }
+		[Required(ErrorMessage = "Insira sua senha")]
+		public required string Senha { get; set; }
+	}
+}
