2ce04b3b036c8d2a63a9ccb59f9895db2771c225
Giovanni <giovannivalieri5@gmail.com>
2025-05-06 13:54:24 -0300

    build: Mapper e repositorios adicionados como dependência

---
+++ backend/Infraestrutura/DependeciesConfig.cs
@@ -0,0 +1,35 @@
+﻿using backend.BancoDeDados;
+using backend.Repositorios;
+using backend.Repositorios.Interface;
+using Microsoft.EntityFrameworkCore;
+using MySql.EntityFrameworkCore.Extensions;
+using Scalar.AspNetCore;
+
+namespace backend.Infraestrutura
+{
+	public static class DependeciesConfig
+	{
+		public static void AddDependecies(this WebApplicationBuilder builder)
+		{
+			builder.Services.AddAutoMapper(typeof(Program));
+			builder.Services.AddDbContext<TarefasDbContext>(opt =>
+				opt.UseMySQL(builder.Configuration.GetConnectionString("MySqlConnection")!));
+			AddRepositorios(builder);
+		}
+
+		public static void AddRepositorios(WebApplicationBuilder builder)
+		{
+			builder.Services.AddScoped<IUsuarioRepositorio, UsuarioRepositorio>();
+		}
+		public static void AddApiDocumentation(this WebApplication app)
+		{
+			app.MapOpenApi();
+			app.MapScalarApiReference("scalar/v1", opt =>
+			{
+				opt.Title = "Tarefas API";
+				opt.Theme = ScalarTheme.BluePlanet;
+				opt.WithDefaultHttpClient(ScalarTarget.Node, ScalarClient.Fetch);
+			});
+		}
+	}
+}
