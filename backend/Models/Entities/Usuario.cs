3399f73350673901aa1694190eadb5438ee2e834
Giovanni <giovannivalieri5@gmail.com>
2025-05-06 09:52:52 -0300

    feat: atributo para verificar a qtd de tarefas adicionado

--- backend/Models/Entities/Usuario.cs
+++ backend/Models/Entities/Usuario.cs
@@ -7,8 +7,9 @@ namespace backend.Models.Entities
 		public required string Nome { get; set; }
 		public required string Email { get; set; }
 		public required string Senha { get; set; }
-		public string? TokenAcesso { get; set; }
-		public string? TokenRecarga { get; set; }
+		public int QuantidadeTarefa { get; set; } = 0
+		public string? TokenAcesso {get; set; }
+		public string? TokenRecarga{ get; set; }
 		public List<Tarefas>? Tarefa {  get; set; }
 	}
 }
