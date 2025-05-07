0f718ec217d59382d86f6146a48be875ac8ab196
Giovanni <giovannivalieri5@gmail.com>
2025-05-07 13:15:19 -0300

    feat: classe para retornar o token de recarga gerado adicionado

---
+++ backend/Models/Tokens/TokenCriado.cs
@@ -0,0 +1,8 @@
+﻿namespace backend.Models.Tokens
+{
+	public class TokenCriado
+	{
+		public required string TokenRecarga {  get; set; }
+		public DateTime? TempoToken { get; set; } = DateTime.UtcNow.AddDays(7);
+	}
+}
