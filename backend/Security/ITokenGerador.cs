885aca91266d240d482e066d9d85d2639da1f451
Giovanni <giovannivalieri5@gmail.com>
2025-05-07 13:17:50 -0300

    feat: interface para gerar tokens jwt adicionada

---
+++ backend/Security/ITokenGerador.cs
@@ -0,0 +1,12 @@
+﻿using backend.Models.Entities;
+using backend.Models.Tokens;
+
+namespace backend.Security
+{
+	public interface ITokenGerador
+	{
+		public string CriarToken(string nome, Guid idUsuario, string email);
+		public TokenCriado CriarTokenRecarga();
+
+	}
+}
