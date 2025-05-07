25693d4d2e0eee75c577b6daf86800a6f918789c
Giovanni <giovannivalieri5@gmail.com>
2025-05-07 16:37:25 -0300

    feat: classe para receber dados para recarregar o token adicionada

---
+++ backend/Models/Tokens/TokenRecargaDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace backend.Models.Tokens
+{
+	public class TokenRecargaDto
+	{
+		[Required(ErrorMessage = "É necessário o Id de usuário")]
+		public required Guid IdUsuario { get; set; }
+
+		[Required(ErrorMessage = "É necessário o Token de recarga")]
+		public required string TokenRecarga { get; set; }
+	}
+}
