d615789411bc8a113cf78d456f434d8200ab23e6
Giovanni <giovannivalieri5@gmail.com>
2025-05-07 13:16:24 -0300

    refactor: Assinatura retorna TokenResponse agora

--- backend/Repositorios/Interface/IUsuarioRepositorio.cs
+++ backend/Repositorios/Interface/IUsuarioRepositorio.cs
@@ -1,12 +1,13 @@
 ﻿using backend.Models.Dtos.UsuarioDto;
 using backend.Models.Entities;
+using backend.Models.Tokens;
 
 namespace backend.Repositorios.Interface
 {
 	public interface IUsuarioRepositorio
 	{
 		public Task<UsuarioResponse> BuscarUsuarioPorId(Guid id);
-		public Task<UsuarioResponse> EntrarNaConta(UsuarioLogin conta);
+		public Task<TokenResponse> EntrarNaConta(UsuarioLogin conta);
 		public Task<UsuarioResponse> EditarConta(Guid id, UsuarioUpdate conta);
 		public Task<UsuarioResponse> CriarConta(UsuarioCreate usuario);
 		public Task ExcluirConta(Guid id);
