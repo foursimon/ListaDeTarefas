92e84de9cd0ee6bd623a584bc8756a92ce3913dd
Giovanni <giovannivalieri5@gmail.com>
2025-05-07 16:38:26 -0300

    feat: atributo TokenAcesso pode ser retornado agora

--- backend/Models/Tokens/TokenCriado.cs
+++ backend/Models/Tokens/TokenCriado.cs
@@ -1,7 +1,11 @@
-﻿namespace backend.Models.Tokens
+﻿using System.Text.Json.Serialization;
+
+namespace backend.Models.Tokens
 {
 	public class TokenCriado
 	{
+		[JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+		public string? TokenAcesso { get; set; } = null;
 		public required string TokenRecarga {  get; set; }
 		public DateTime? TempoToken { get; set; } = DateTime.UtcNow.AddDays(7);
 	}
