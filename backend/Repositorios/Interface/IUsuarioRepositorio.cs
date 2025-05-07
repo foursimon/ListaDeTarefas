e436432d42de6aad7617f04cf749cb905a00b4d3
Giovanni <giovannivalieri5@gmail.com>
2025-05-07 16:40:43 -0300

    feat: assinatura para recarregar token adicionada

--- backend/Repositorios/Interface/IUsuarioRepositorio.cs
+++ backend/Repositorios/Interface/IUsuarioRepositorio.cs
@@ -10,6 +10,7 @@ namespace backend.Repositorios.Interface
 		public Task<TokenResponse> EntrarNaConta(UsuarioLogin conta);
 		public Task<UsuarioResponse> EditarConta(Guid id, UsuarioUpdate conta);
 		public Task<UsuarioResponse> CriarConta(UsuarioCreate usuario);
+		public Task<TokenResponse> RecarregarToken(Guid idUsuario, string tokenRecarga);
 		public Task ExcluirConta(Guid id);
 	}
 }
