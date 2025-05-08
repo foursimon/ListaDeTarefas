c70c156236fd16cd30385ac4e85db50d3ea1bcda
Giovanni <giovannivalieri5@gmail.com>
2025-05-08 09:49:15 -0300

    refactor: assinatura apenas para manipulação de banco de dados

--- backend/Repositorios/Interface/IUsuarioRepositorio.cs
+++ backend/Repositorios/Interface/IUsuarioRepositorio.cs
@@ -6,11 +6,12 @@ namespace backend.Repositorios.Interface
 {
 	public interface IUsuarioRepositorio
 	{
-		public Task<UsuarioResponse> BuscarUsuarioPorId(Guid id);
-		public Task<TokenResponse> EntrarNaConta(UsuarioLogin conta);
-		public Task<UsuarioResponse> EditarConta(Guid id, UsuarioUpdate conta);
-		public Task<UsuarioResponse> CriarConta(UsuarioCreate usuario);
-		public Task<TokenResponse> RecarregarToken(Guid idUsuario, string tokenRecarga);
-		public Task ExcluirConta(Guid id);
+		public Task<Usuario> BuscarUsuarioPorId(Guid id);
+		public Task<Usuario> BuscarUsuarioPorEmail(string email);
+		public Task<Usuario> ArmazenarNovoUsuario(Usuario dados);
+
+		public Task<Usuario> AtualizarUsuario(Usuario usuario);
+
+		public Task DeletarUsuario(Guid id);
 	}
 }
