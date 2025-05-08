6e89a2f108f42810d1f9f13d540285f88bc1af29
Giovanni <giovannivalieri5@gmail.com>
2025-05-08 11:41:07 -0300

    feat: DTOs para tarefas adicionado

---
+++ backend/Models/Dtos/TarefasDto/TarefasUpdate.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace backend.Models.Dtos.TarefasDto
+{
+	public class TarefasUpdate
+	{
+		public string? Titulo { get; set; }
+		public DateOnly? DataDeEncerramento { get; set; }
+		public string? Descricao { get; set; }
+		public string? Tipo { get; set; }
+	}
+}
