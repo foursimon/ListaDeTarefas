90e0d7c7187857445e18f203233b838c575455d0
Giovanni <giovannivalieri5@gmail.com>
2025-05-08 11:38:25 -0300

    docs: reestruturação de arquivos de exceção

---
+++ backend/Exceptions/UsuarioException/TokenInvalidoException.cs
@@ -0,0 +1,12 @@
+﻿namespace backend.Exceptions.UsuarioException
+{
+	public class TokenInvalidoException : Exception
+	{
+		public TokenInvalidoException()
+		{
+		}
+
+		public TokenInvalidoException(string? message)
+			: base(message) { }
+	}
+}
