abea0214954b9498d38913091a19c51c43d4f8b0
Giovanni <giovannivalieri5@gmail.com>
2025-05-09 13:38:19 -0300

    feat: dto para criar e atualizar item de lista adicioando

--- backend/Models/Dtos/CheckItemDtos.cs
+++ backend/Models/Dtos/CheckItemDtos.cs
@@ -1,8 +1,19 @@
-﻿namespace backend.Models.Dtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace backend.Models.Dtos
 {
 	public sealed record CheckItemResponse(
 		Guid Id,
 		string Item,
 		bool Concluido
 	);
-}
+
+	public sealed record CheckItemCreate(
+		[Required(ErrorMessage = "Escreva o item da lista")]
+		string item
+	);
+
+	public sealed record CheckItemUpdate(
+		string? item	
+	);
+};
