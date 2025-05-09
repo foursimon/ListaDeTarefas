a42790d745a5e0a0455c59692aa3c30f4b685715
Giovanni <giovannivalieri5@gmail.com>
2025-05-08 22:33:32 -0300

    feat: interface para repositório de tarefas adicionado

---
+++ backend/Repositorios/Interface/ITarefasRepositorio.cs
@@ -0,0 +1,18 @@
+﻿using backend.Models.Dtos;
+using backend.Models.Entities;
+
+namespace backend.Repositorios.Interface
+{
+	public interface ITarefasRepositorio
+	{
+		public Task<List<Tarefas>> BuscarTarefasPorUsuario(Guid idUsuario);
+		public Task<Tarefas> CriarTarefa(Tarefas novaTarefa);
+
+		public Task<Tarefas> AtualizarTarefa(Guid idTarefa, TarefasUpdate tarefa);
+
+		public Task<Tarefas> AtualizarStatusTarefa(Guid idTarefa, bool status);
+		public Task ExcluirTarefa(Guid idTarefa);
+
+
+	}
+}
