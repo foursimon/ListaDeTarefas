2f3bdd0b878104e1c890a0bd71bf9193cf6ab321
Giovanni <giovannivalieri5@gmail.com>
2025-05-08 22:40:16 -0300

    refactor: using ajustado

--- backend/Controllers/UsuarioController.cs
+++ backend/Controllers/UsuarioController.cs
@@ -1,9 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using backend.Models.Dtos.UsuarioDto;
 using Microsoft.AspNetCore.Authorization;
 using backend.Models.Tokens;
 using backend.Services.Interface;
 using backend.Exceptions.UsuarioException;
+using backend.Models.Dtos;
 
 namespace backend.Controllers
 {
