3e5fca4a569d5367e3e64a134b7d53d07a4856ad
Giovanni <giovannivalieri5@gmail.com>
2025-05-08 22:28:02 -0300

    feat: mapeador manual de dados de itens de lista de tarefas adicionado

---
+++ backend/Infraestrutura/Mappers/CheckItemMapperExtensions.cs
@@ -0,0 +1,17 @@
+﻿using backend.Models.Dtos;
+using backend.Models.Entities;
+
+namespace backend.Infraestrutura.Mappers
+{
+	public static class CheckItemMapperExtensions
+	{
+		public static CheckItemResponse ToCheckItemResponse(this CheckItem item)
+		{
+			return new CheckItemResponse(
+				item.Id,
+				item.Item,
+				item.Concluido
+			);
+		}
+	}
+}
