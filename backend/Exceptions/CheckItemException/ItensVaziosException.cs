60679a61892bdde80cafac2ccf57f0ecadd38688
Giovanni <giovannivalieri5@gmail.com>
2025-05-10 19:06:06 -0300

    feat: exceções de itens de lista adicionadas

---
+++ backend/Exceptions/CheckItemException/ItensVaziosException.cs
@@ -0,0 +1,10 @@
+﻿namespace backend.Exceptions.CheckItemException
+{
+	public class ItensVaziosException : ArgumentException
+	{
+		public ItensVaziosException(){}
+
+		public ItensVaziosException(string? mensagem) 
+			: base(mensagem){}
+	}
+}
