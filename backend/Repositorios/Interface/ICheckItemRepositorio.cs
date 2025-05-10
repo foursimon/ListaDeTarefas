5f39c11a773c9789d956288e9267087ba8b580b9
Giovanni <giovannivalieri5@gmail.com>
2025-05-10 18:58:30 -0300

    feat: interface para manipular tabela de itens adicionada

---
+++ backend/Repositorios/Interface/ICheckItemRepositorio.cs
@@ -0,0 +1,11 @@
+﻿using backend.Models.Entities;
+
+namespace backend.Repositorios.Interface
+{
+	public interface ICheckItemRepositorio
+	{
+		public Task<List<CheckItem>> AdicionarItensNaLista(Guid idTarefa, List<CheckItem> itens);
+		public Task<List<CheckItem>> BuscarItensPorId(List<Guid> idItens);
+		public Task<List<CheckItem>> EditarItens(Guid idTarefa, List<CheckItem> itens);
+	}
+}
