228ed447195fd2c7ad6880b513cce0f30437dedb
Giovanni <giovannivalieri5@gmail.com>
2025-05-11 16:57:58 -0300

    remove: classes tokens agora estão no arquivo TokenDtos

--- backend/Models/Tokens/TokenResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace backend.Models.Tokens
-{
-	public class TokenResponse
-	{
-		public required string TokenAcesso { get; set; }
-		public required string TokenRecarga { get; set; }
-
-	}
-}
