f15b4e3577e1de092b015b592a89c2c06ee15563
Giovanni <giovannivalieri5@gmail.com>
2025-05-11 16:59:01 -0300

    refactor: ajustando métodos para se adequar a classe TokensDtos

--- backend/Services/UsuarioService.cs
+++ backend/Services/UsuarioService.cs
@@ -2,7 +2,6 @@
 using backend.Infraestrutura.Mappers;
 using backend.Models.Dtos;
 using backend.Models.Entities;
-using backend.Models.Tokens;
 using backend.Repositorios.Interface;
 using backend.Security;
 using backend.Services.Interface;
@@ -14,21 +13,31 @@ namespace backend.Services
 		ISenhaHasher criptografia, ITokenGerador geradorToken, 
 		IHttpContextAccessor httpContext) : IUsuarioService
 	{
+		//Método responsável por armazenar informações em Cookies
 		public void ColocarTokensNoCookie(TokenResponse tokens)
 		{
+			//Aqui estou armazenando o token de acesso no cookie
 			httpContext.HttpContext!.Response.Cookies.Append("TOKEN_ACESSO", tokens.TokenAcesso,
 				new CookieOptions
 				{
+					//Expires indica o tempo que o cookie irá expirar,
 					Expires = DateTime.UtcNow.AddMinutes(20),
+					//HttpOnly indica se o valor no cookie não pode ser acessado
+					//por scripts no lado cliente (frontend)
 					HttpOnly = true,
+					//IsEssential indica que o valor que está sendo armazenado no cookie
+					//é essencial para o funcionamento correto do sistema
 					IsEssential = true,
+					//Secure indica se o cookie só pode ser transmitido em conexão
+					//HTTPS.
 					Secure = true,
+					//SameSite indica se o cookie pode ser usado entre sites
 					SameSite = SameSiteMode.Strict
 				});
 			httpContext.HttpContext!.Response.Cookies.Append("TOKEN_RECARGA", tokens.TokenRecarga,
 				new CookieOptions
 				{
-					Expires = DateTime.UtcNow.AddMinutes(20),
+					Expires = DateTime.UtcNow.AddDays(7),
 					HttpOnly = true,
 					IsEssential = true,
 					Secure = true,
@@ -69,39 +78,34 @@ namespace backend.Services
 			//Um novo token de recarga é atribuído para o usuário quando
 			//ele entra na sua conta manualmente.
 			AtribuirTokenRecarga(usuario);
-			TokenResponse tokenResposta = new TokenResponse()
-			{
-				TokenAcesso = geradorToken.CriarToken(usuario.Nome,
-					usuario.Id, usuario.Email),
-				TokenRecarga = usuario.TokenRecarga!
-			};
+			var tokenAcesso = geradorToken.CriarToken(usuario.Nome,
+				usuario.Id, usuario.Email);
+			var tokenRecarga = usuario.TokenRecarga!;
 			//Atualizo as informações salva no banco de dados
 			//com o token de recarga criado.
 			await usuarioRepositorio.AtualizarUsuario(usuario);
-			return tokenResposta;
+			return new TokenResponse(tokenAcesso, tokenRecarga);
 		}
 
 		public async Task ExcluirConta()
 		{
-			Guid id = Guid.Parse(httpContext.HttpContext?.User.FindFirstValue(ClaimTypes.NameIdentifier)!);
+			Guid id = Guid.Parse(httpContext.HttpContext!.User.FindFirstValue(ClaimTypes.NameIdentifier)!);
 			await usuarioRepositorio.DeletarUsuario(id);
 			return;
 		}
 		public async Task<TokenResponse> RecarregarToken()
 		{
 			var idUsuario = Guid.Parse(httpContext.HttpContext!.User.FindFirstValue(ClaimTypes.NameIdentifier)!);
-			var tokenRecarga = httpContext.HttpContext!.Request.Cookies["TOKEN_Recarga"];
+			var tokenRecargaCookie = httpContext.HttpContext!.Request.Cookies["TOKEN_Recarga"];
 			Usuario usuario = await usuarioRepositorio.BuscarUsuarioPorId(idUsuario);
 
-			TokenCriado tokenNovo = geradorToken.RecarregarToken(usuario, tokenRecarga!);
+			TokenCriado tokenNovo = geradorToken.RecarregarToken(usuario, tokenRecargaCookie!);
 			usuario.TokenRecarga = tokenNovo.TokenRecarga;
 			usuario.TempoToken = tokenNovo.TempoToken;
 			await usuarioRepositorio.AtualizarUsuario(usuario);
-			return new TokenResponse()
-			{
-				TokenAcesso = tokenNovo.TokenAcesso!,
-				TokenRecarga = tokenNovo.TokenRecarga
-			};
+			var tokenAcesso = tokenNovo.TokenAcesso!;
+			var tokenRecarga = tokenNovo.TokenRecarga;
+			return new TokenResponse(tokenAcesso, tokenRecarga);
 		}
 
 		//Método responsável por pegar o token de recarga
