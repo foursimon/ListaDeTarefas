a85790930729ea10cf5463952387d8050b127aae
Giovanni <giovannivalieri5@gmail.com>
2025-05-12 17:00:56 -0300

    feat: método para pegar id do usuário pelo token jwt adicionado

--- backend/Security/ITokenGerador.cs
+++ backend/Security/ITokenGerador.cs
@@ -6,8 +6,10 @@ namespace backend.Security
 	public interface ITokenGerador
 	{
 		public string CriarToken(string nome, Guid idUsuario, string email);
-		public TokenCriado CriarTokenRecarga();
+		public TokenCriado CriarTokenRecarga(Guid idUsuario);
 		public TokenCriado RecarregarToken(Usuario usuario, string tokenRecebido);
+
+		public Guid PegarIdUsuarioToken(string token);
 
 	}
 }
