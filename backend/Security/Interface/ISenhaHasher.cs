0e2f39220964865e47753d14eec257cf19377789
Giovanni <giovannivalieri5@gmail.com>
2025-05-16 18:38:46 -0300

    refactor: interfaces adicionadas em sua própria pasta

---
+++ backend/Security/Interface/ISenhaHasher.cs
@@ -0,0 +1,9 @@
+﻿namespace backend.Security
+{
+	public interface ISenhaHasher
+	{
+		public string CriptografarSenha(string senha);
+
+		public bool VerificarSenha(string senha, string senhaCriptografada);
+	}
+}
---
+++ backend/Security/Interface/ITokenGerador.cs
@@ -0,0 +1,15 @@
+﻿using backend.Models.Entities;
+using backend.Models.Dtos;
+
+namespace backend.Security.Interface
+{
+	public interface ITokenGerador
+	{
+		public string CriarToken(string nome, Guid idUsuario, string email);
+		public TokenCriado CriarTokenRecarga(Guid idUsuario);
+		public TokenCriado RecarregarToken(Usuario usuario, string tokenRecebido);
+
+		public Guid PegarIdUsuarioToken(string token);
+
+	}
+}
