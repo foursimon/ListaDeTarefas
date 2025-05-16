b925a3ed303b313f025385628560c4ee0237362b
Giovanni <giovannivalieri5@gmail.com>
2025-05-16 18:41:43 -0300

    remove: arquivos movidos para pasta Interface

--- backend/Security/ISenhaHasher.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace backend.Security
-{
-	public interface ISenhaHasher
-	{
-		public string CriptografarSenha(string senha);
-
-		public bool VerificarSenha(string senha, string senhaCriptografada);
-	}
-}
