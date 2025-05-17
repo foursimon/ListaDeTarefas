10eabb7c8ba4682a2b71e75e07b154ed10ee34fd
Giovanni <giovannivalieri5@gmail.com>
2025-05-17 12:08:42 -0300

    refactor: tokens JWT não são retornados e busca de dados do usuário adicionada

--- backend/Services/Interface/IUsuarioService.cs
+++ backend/Services/Interface/IUsuarioService.cs
@@ -4,14 +4,13 @@ namespace backend.Services.Interface
 {
 	public interface IUsuarioService
 	{
-		public Task<TokenResponse> EntrarNaConta(UsuarioLogin conta);
+		public Task EntrarNaConta(UsuarioLogin conta);
+		public Task<UsuarioResponse> BuscarInformacoesDoUsuario(); 
 		public Task<UsuarioResponse> EditarConta(UsuarioUpdate conta);
 		public Task<UsuarioResponse> CriarConta(UsuarioCreate usuario);
 
 		public Task SairDaConta();
-		public Task<TokenResponse> RecarregarToken();
-
-		public void ColocarTokensNoCookie(TokenResponse tokens);
+		public Task RecarregarToken();
 		public Task ExcluirConta();
 	}
 }
