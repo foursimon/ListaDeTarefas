c11cb909e0fb1b7f15b369c8b3183a2d4efbfed4
Giovanni <giovannivalieri5@gmail.com>
2025-05-20 11:43:04 -0300

    refactor: exceções esperadas foram removidas

--- backend/Repositorios/UsuarioRepositorio.cs
+++ backend/Repositorios/UsuarioRepositorio.cs
@@ -1,5 +1,4 @@
 ﻿using backend.BancoDeDados;
-using backend.Exceptions.UsuarioException;
 using backend.Models.Entities;
 using backend.Repositorios.Interface;
 using Microsoft.EntityFrameworkCore;
@@ -10,7 +9,7 @@ namespace backend.Repositorios
 	public class UsuarioRepositorio
 		(TarefasDbContext context) : IUsuarioRepositorio
 	{
-		public async Task<Usuario> ArmazenarNovoUsuario(Usuario dados)
+		public async Task<Usuario?> ArmazenarNovoUsuario(Usuario dados)
 		{
 			context.Usuarios.Add(dados);
 			try
@@ -20,34 +19,29 @@ namespace backend.Repositorios
 			}catch(DbUpdateException ex)
 				when(ex.InnerException is MySqlException { Number: (int)MySqlErrorCode.DuplicateKeyEntry })
 			{
-				throw new EmailJaExisteException("O e-mail informado já está vinculado a uma conta");
+				return null;
 			}
 			return dados;
 		}
 
-		public async Task<Usuario> BuscarUsuarioPorEmail(string email)
+		public async Task<Usuario?> BuscarUsuarioPorEmail(string email)
 		{
-			Usuario usuario = await context.Usuarios.FirstOrDefaultAsync(p => p.Email == email) 
-				?? throw new UsuarioNaoEncontradoException($"Usuário com email {email} não foi encontrado");
-			return usuario;
+			return await context.Usuarios.FirstOrDefaultAsync(p => p.Email == email); ;
 		}
 
-		public async Task<Usuario> BuscarUsuarioPorId(Guid id)
+		public async Task<Usuario?> BuscarUsuarioPorId(Guid id)
 		{
-			Usuario usuario = await context.Usuarios.FindAsync(id)
-				?? throw new UsuarioNaoEncontradoException($"Usuario com id {id} não foi encontrado");
-			return usuario;
+			return await context.Usuarios.FindAsync(id);
 		}
 
-		public async Task DeletarUsuario(Guid id)
+		public async Task DeletarUsuario(Usuario usuario)
 		{
-			Usuario usuario = await BuscarUsuarioPorId(id);
 			context.Usuarios.Remove(usuario);
 			await context.SaveChangesAsync();
 			return;
 		}
 
-		public async Task<Usuario> AtualizarUsuario(Usuario usuario)
+		public async Task<Usuario?> AtualizarUsuario(Usuario usuario)
 		{
 			await BuscarUsuarioPorId(usuario.Id);
 			context.Usuarios.Update(usuario);
@@ -58,14 +52,13 @@ namespace backend.Repositorios
 			catch (DbUpdateException ex)
 				when (ex.InnerException is MySqlException { Number: (int)MySqlErrorCode.DuplicateKeyEntry })
 			{
-				throw new EmailJaExisteException("O e-mail informado já está vinculado a uma conta");
+				return null;
 			}
 			return usuario;
 		}
 
-		public async Task ExcluirTokenRecarga(Guid id)
+		public async Task ExcluirTokenRecarga(Usuario usuario)
 		{
-			Usuario usuario = await BuscarUsuarioPorId(id);
 			usuario.TokenRecarga = null;
 			usuario.TempoToken = null;
 			context.Usuarios.Update(usuario);
