645b6b3f5231352c8cdb039ca78a2f7eeced0a80
Giovanni <giovannivalieri5@gmail.com>
2025-05-20 11:35:31 -0300

    feat: mapeador para ProblemDetails adicionado

---
+++ backend/Infraestrutura/ProblemExtensions.cs
@@ -0,0 +1,20 @@
+﻿using backend.Resultados;
+using Microsoft.AspNetCore.Mvc;
+using Org.BouncyCastle.Ocsp;
+
+namespace backend.Infraestrutura
+{
+	public static class ProblemExtensions
+	{
+		public static ProblemDetails ToProblemDetails(this Error erro)
+		{
+			return new ProblemDetails
+			{
+				Type = $"https://developer.mozilla.org/pt-BR/docs/Web/HTTP/Reference/Status/{erro.CodigoStatus}",
+				Title = erro.Titulo,
+				Detail = erro.Descricao,
+				Status = erro.CodigoStatus
+			};
+		}
+	}
+}
