78914714d55acf5a17752f7fa2ff9ab93b341db1
Giovanni <giovannivalieri5@gmail.com>
2025-05-20 11:36:16 -0300

    feat: classe Result para retornar objetos de resultados

---
+++ backend/Resultados/Result.cs
@@ -0,0 +1,28 @@
+﻿namespace backend.Resultados
+{
+	public class Result<TValue, TError>
+	{
+		public bool IsSuccess { get; }
+		public TValue? Value { get; }
+		public TError? Error { get; }
+
+		private Result(TValue? value)
+		{
+			IsSuccess = true;
+			Value = value;
+			Error = default;
+		}
+		private Result(TError? error)
+		{
+			IsSuccess = false;
+			Value = default;
+			Error = error;
+		}
+
+
+		public static implicit operator Result<TValue, TError>(TValue value) => 
+			new Result<TValue, TError>(value);
+		public static implicit operator Result<TValue, TError>(TError error) => 
+			new Result<TValue, TError>(error);
+	}
+}
