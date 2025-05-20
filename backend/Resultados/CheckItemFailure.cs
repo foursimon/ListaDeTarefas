2d7438d798ed173e6910c54f7b490f643d8d91aa
Giovanni <giovannivalieri5@gmail.com>
2025-05-20 11:37:32 -0300

    feat: tipos de erros de itens de lista adicionados

---
+++ backend/Resultados/CheckItemFailure.cs
@@ -0,0 +1,12 @@
+﻿namespace backend.Resultados
+{
+	public static class CheckItemFailure
+	{
+		public static Error ItemNaoEncontrado => Error.NotFound
+			("Item não foi encontrado", "Um ou mais itens recebidos não foram encontrados");
+
+		public static Error ItensVazios => Error.BadRequest
+			("Não é possível adicionar itens vazio na lista", 
+			"Itens vazios não podem ser adicionados na lista");
+	}
+}
